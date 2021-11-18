fd8eeb05c141cf88147db8c25603fdb6f3085e17
Pitec7 <chenxi.lin@hotmail.ch>
2021-11-18 11:01:30 +0100

    service worker done

--- js/app.js
+++ js/app.js
@@ -1,8 +1,33 @@
+/*
 // Check if the sw is supported
 if('serviceWorker' in navigator){
-
-    navigator.serviceWorker.register('/sw.js')
+   
+    navigator.serviceWorker.register('../sw.js')
     .then((reg) => console.log('service worker registered', reg))
     .catch((err) => console.log('service worker not registered', err))
     
+
+    navigator.serviceWorker.register('/sw.js').then(function(registration){
+        console.log('service worker registration succeeded:', registration);
+    },
+    function(error){
+        console.log('service worker registration failed:', error);
+    });    
+}else{
+    console.log('service workers are not supported.');
+}
+*/
+
+const swURL = "sw.js";
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register(swURL).then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
 }
